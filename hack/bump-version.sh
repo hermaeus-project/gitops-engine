41b0d9da4e0eeb6832026c81c81092e31e010150
Robin LIORET <robin.lioret@darylsocialsoftware.com>
2025-11-20 16:31:17 +0100

    feat: initialize github workflows
    Signed-off-by: Robin LIORET <robin.lioret@darylsocialsoftware.com>

---
+++ hack/bump-version.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# hack/bump-version.sh
+# Usage: ./hack/bump-version.sh <new_version>
+# Bumps manifest versions in .version
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
+
+NEW_VERSION="$1"
+
+# Portable sed replacement: write to temp file, then move back
+sed_replace() {
+  local pattern="$1"
+  local file="$2"
+  local tmpfile="${file}.tmp.$$"
+  sed "$pattern" "$file" > "$tmpfile" && mv "$tmpfile" "$file"
+}
+
+# Replace all occurrences of vX.Y.Z with the new version (vNEW_VERSION)
+#sed_replace "s/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/v$NEW_VERSION/g" docs/getting-started.md
+#sed_replace "s/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/v$NEW_VERSION/g" docs/tutorial-argocd-apps.md
+#sed_replace "s/download\/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/download\/v$NEW_VERSION/g" docs/argocd-integrations.md
+#sed_replace "s/gitops-promoter_[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}_checksums\.txt/gitops-promoter_${NEW_VERSION}_checksums.txt/g" docs/argocd-integrations.md
+
+# Replace all occurrences of vX.Y.Z with the new version (vNEW_VERSION)
+sed_replace "s/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/v$NEW_VERSION/g" .version
+
+echo "Bumped manifest versions to v$NEW_VERSION in docs."
